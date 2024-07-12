b99df82a47f12e6265dc58710fae669d250f3af4
sariru@gmail.com <89619301+itsafuu@users.noreply.github.com>
2024-07-12 18:02:38 -0400

    Holepunch (RCUtR) initial files added, this works in tandem with circuit relay to holepunch

---
+++ include/libp2p/protocol/holepunch/holepunch_msg_processor.hpp
@@ -0,0 +1,106 @@
+#ifndef LIBP2P_RELAY_MSG_PROCESSOR_HPP
+#define LIBP2P_RELAY_MSG_PROCESSOR_HPP
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+#include <gsl/span>
+#include <libp2p/connection/stream.hpp>
+#include <libp2p/crypto/key_marshaller.hpp>
+#include <libp2p/host/host.hpp>
+#include <libp2p/log/logger.hpp>
+#include <libp2p/multi/multiaddress.hpp>
+#include <libp2p/network/connection_manager.hpp>
+#include <libp2p/outcome/outcome.hpp>
+#include <libp2p/peer/identity_manager.hpp>
+#include <libp2p/peer/peer_id.hpp>
+#include <libp2p/protocol/identify/observed_addresses.hpp>
+
+namespace holepunch::pb {
+  class HolePunch;
+}
+
+namespace libp2p::protocol {
+  /**
+   * Processor of messages of Autonat protocol
+   */
+  class HolepunchMessageProcessor
+      : public std::enable_shared_from_this<HolepunchMessageProcessor> {
+    using StreamSPtr = std::shared_ptr<connection::Stream>;
+
+   public:
+    using HolepunchCallback = void(const bool &);
+
+    HolepunchMessageProcessor(
+        Host &host, network::ConnectionManager &conn_manager,
+        peer::IdentityManager &identity_manager,
+        std::shared_ptr<crypto::marshaller::KeyMarshaller> key_marshaller);
+
+    boost::signals2::connection onHolepunchReceived(
+        const std::function<HolepunchCallback> &cb);
+
+    /**
+     * Send an autonat message over the provided stream
+     * @param stream to be identified over
+     */
+    void sendHolepunch(StreamSPtr stream);
+
+    /**
+     * Receive an Autonat message from the provided stream
+     * @param stream to be identified over
+     */
+    void receiveHolepunch(StreamSPtr stream);
+
+    /**
+     * Get a Host of this processor
+     * @return Host
+     */
+    Host &getHost() const noexcept;
+
+    /**
+     * Get a ConnectionManager of this processor
+     * @return ConnectionManager
+     */
+    network::ConnectionManager &getConnectionManager() const noexcept;
+
+    /**
+     * Get an ObservedAddresses of this processor
+     * @return ObservedAddresses
+     */
+    const ObservedAddresses &getObservedAddresses() const noexcept;
+
+   private:
+    /**
+     * Called, when an autonat message is written to the stream
+     * @param written_bytes - how much bytes were written
+     * @param stream with the other side
+     */
+    void holepunchSent(outcome::result<size_t> written_bytes,
+                      const StreamSPtr &stream);
+
+    /**
+     * Called, when an autonat message is received from the other peer
+     * @param msg, which was read
+     * @param stream, over which it was received
+     */
+    void holepunchReceived(outcome::result<holepunch::pb::HolePunch> msg_res,
+                          const StreamSPtr &stream);
+
+
+    Host &host_;
+    network::ConnectionManager &conn_manager_;
+    peer::IdentityManager &identity_manager_;
+    std::shared_ptr<crypto::marshaller::KeyMarshaller> key_marshaller_;
+    ObservedAddresses observed_addresses_;
+    boost::signals2::signal<HolepunchCallback> signal_holepunch_received_;
+    int successful_addresses_;
+    int unsuccessful_addresses_;
+
+
+    log::Logger log_ = log::createLogger("HolepunchMsgProcessor");
+  };
+}
+
+#endif
---
+++ src/protocol/holepunch/holepunch.cpp
@@ -0,0 +1,103 @@
+#include <libp2p/protocol/holepunch/holepunch.hpp>
+
+#include <string>
+#include <tuple>
+
+#include <boost/assert.hpp>
+#include <iostream>
+
+
+namespace {
+  const std::string kHolepunchProto = "/libp2p/dcutr";
+}  // namespace
+
+namespace libp2p::protocol {
+    Holepunch::Holepunch(Host &host,
+                     std::shared_ptr<HolepunchMessageProcessor> msg_processor,
+                     event::Bus &event_bus)
+      : host_{host}, msg_processor_{std::move(msg_processor)}, bus_{event_bus} {
+    BOOST_ASSERT(msg_processor_);
+    msg_processor_->onHolepunchReceived([this](const bool& status) {
+        natstatus_ = status;
+        });
+  }
+
+  boost::signals2::connection Holepunch::onHolepunchReceived(
+      const std::function<HolepunchMessageProcessor::HolepunchCallback> &cb) {
+    return msg_processor_->onHolepunchReceived(cb);
+  }
+
+  std::vector<multi::Multiaddress> Holepunch::getAllObservedAddresses() const {
+    return msg_processor_->getObservedAddresses().getAllAddresses();
+  }
+
+  std::vector<multi::Multiaddress> Holepunch::getObservedAddressesFor(
+      const multi::Multiaddress &address) const {
+    return msg_processor_->getObservedAddresses().getAddressesFor(address);
+  }
+
+  peer::Protocol Holepunch::getProtocolId() const {
+    return kHolepunchProto;
+  }
+
+  void Holepunch::handle(StreamResult stream_res) {
+    if (!stream_res) {
+      return;
+    }
+    msg_processor_->receiveHolepunch(std::move(stream_res.value()));
+  }
+
+  void Holepunch::start() {
+    // no double starts
+    BOOST_ASSERT(!started_);
+    started_ = true;
+
+    host_.setProtocolHandler(
+        kHolepunchProto,
+        [wp = weak_from_this()](protocol::BaseProtocol::StreamResult rstream) {
+          if (auto self = wp.lock()) {
+            self->handle(std::move(rstream));
+          }
+        });
+
+    sub_ = bus_.getChannel<event::network::OnNewConnectionChannel>().subscribe(
+        [wp = weak_from_this()](auto &&conn) {
+          if (auto self = wp.lock()) {
+            return self->onNewConnection(conn);
+          }
+        });
+  }
+
+  void Holepunch::onNewConnection(
+      const std::weak_ptr<connection::CapableConnection> &conn) {
+    if (conn.expired()) {
+      return;
+    }
+
+    auto remote_peer_res = conn.lock()->remotePeer();
+    if (!remote_peer_res) {
+      return;
+    }
+
+    auto remote_peer_addr_res = conn.lock()->remoteMultiaddr();
+    if (!remote_peer_addr_res) {
+      return;
+    }
+
+    peer::PeerInfo peer_info{std::move(remote_peer_res.value()),
+                             std::vector<multi::Multiaddress>{
+                                 std::move(remote_peer_addr_res.value())}};
+
+    msg_processor_->getHost().newStream(
+        peer_info, kHolepunchProto,
+        [self{shared_from_this()}](auto &&stream_res) {
+            if (!stream_res) {
+                self->log_->error("Failed to create new stream: {}", stream_res.error().message());
+                return;
+            }
+            self->log_->info("Sending Autonat request to peer");
+            auto stream = stream_res.value();
+            self->msg_processor_->sendHolepunch(stream);
+        });
+  }
+}
